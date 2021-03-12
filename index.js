177c2142d9e924e926f5a13b2bb8f1d0ee603617
Charabamba <voralse@mail.ru>
2021-03-12 14:38:33 +0500

    add cross-browser prefixes

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ let phoneInput = document.getElementById("phone");
 IMask(phoneInput, {
   mask: "+{0}(000)000-00-00",
 });
-
-let sendButton = document.querySelector(".feedback__button");
-
-sendButton.addEventListener("onsubmit", () => {});
